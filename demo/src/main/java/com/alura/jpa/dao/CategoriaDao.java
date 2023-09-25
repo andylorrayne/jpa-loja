e3ed15fc25983ec20a556321dbd4f8a115813ebe
andylorrayne <alorraynefigueiredo@gmail.com>
2023-09-25 13:46:21 -0300

    Inclusão de metodos na CategoriaDAO

--- demo/src/main/java/com/alura/jpa/dao/CategoriaDao.java
+++ demo/src/main/java/com/alura/jpa/dao/CategoriaDao.java
@@ -14,4 +14,16 @@ public class CategoriaDao {
     public void cadastrar(Categoria categoria){
         this.em.persist(categoria);
     }
+
+    public void atualizar(Categoria categoria){
+        this.em.merge(categoria);
+    }
+
+    // não pode remover entidade que esteja em detached
+    public void remover(Categoria categoria){
+        categoria = em.merge(categoria); //usar o marge requer que você reatribuir
+        this.em.remove(categoria);
+    }
+
+
 }
